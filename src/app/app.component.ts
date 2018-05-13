1c98bdb426ce594d2d369729fa731a1d88b01e87
Durgaachandrakala <durgaachandrakala@gmail.com>
2018-05-13 10:23:44 +0530

    speech-to-text integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import SpeechToText from 'speech-to-text';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() {
+    const onAnythingSaid = text => console.log(`Interim text: ${text}`);
+    const onFinalised = text => console.log(`Finalised text: ${text}`);
+
+    try {
+      const listener = new SpeechToText(onAnythingSaid, onFinalised);
+      listener.startListening();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
